137c650161111a370d3164e258fd51dc73796a83
Van Chien <vanchien270901@gmail.com>
2021-12-27 11:39:44 +0700

    add-databasse

---
+++ ArtGallery.Data/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery.Data.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public List<ProductInCart> ProductInCarts { get; set; }
+    }
+}
