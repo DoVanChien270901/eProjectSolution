a6ac55fd87a291970b78aa2e6756016a2451b4a6
Van Chien <vanchien270901@gmail.com>
2021-12-28 21:28:56 +0700

    day1

---
+++ ArtGallery.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using ArtGallary.Application.System.Users;
+using ArtGallery.ViewModel.System.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtGallery.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            //if (!ModelState.IsValid) return BadRequest();
+            var resultToken = await _userService.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken)) return BadRequest(resultToken);
+            return Ok(new { token = resultToken });
+        }
+    }
+}
